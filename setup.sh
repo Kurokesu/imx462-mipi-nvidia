eb540c33e013bc21b87f59e8cea5692b50bf1fd0
DaniusKalv <danius@kurokesu.com>
2025-11-28 17:21:47 +0200

    Setup driver module to be loaded at boot

--- setup.sh
+++ setup.sh
@@ -12,4 +12,9 @@ cp $DRV_FILES_DIR/tegra234-p3767-camera-p3768-imx462-A.dtbo /boot
 echo "Copying camera calibration to /var/nvidia/nvcam/settings"
 cp ./tuning/camera_overrides.isp /var/nvidia/nvcam/settings
 
+echo "Installing kernel module"
+cp $DRV_FILES_DIR/nv_imx462.ko /lib/modules/$(uname -r)/extra
+depmod
+echo nv_imx462 | tee /etc/modules-load.d/kurokesu.conf
+
 echo "Sucess! Don't forget to run \"sudo /opt/nvidia/jetson-io/jetson-io.py\""
